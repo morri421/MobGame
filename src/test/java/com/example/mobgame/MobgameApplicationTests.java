da3f9ab7b5bb2bafafec4c20c68cacd3b44ab568
Devon Morrison <devmorri421@gmail.com>
2020-06-03 21:17:36 -0400

    First commit

---
+++ src/test/java/com/example/mobgame/MobgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

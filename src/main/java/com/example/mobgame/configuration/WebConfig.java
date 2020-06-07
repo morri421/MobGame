21ced4fa3a4c52e34be7e95fe89fc4671f614bcd
Devon Morrison <devmorri421@gmail.com>
2020-06-07 17:36:47 -0400

    Test commit for Jenkins build

--- src/main/java/com/example/mobgame/configuration/WebConfig.java
+++ src/main/java/com/example/mobgame/configuration/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
 
+    private String test;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
